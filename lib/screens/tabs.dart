a0137782db321e495195caccdc51c4daaff81850
Jessica Ernst <jessica_ernst@outlook.de>
2024-12-24 00:28:02 +0100

    add snackbar to favorites

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -14,6 +14,15 @@ class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExistingFavorite = _favoriteMeals.contains(meal);
 
@@ -21,9 +30,11 @@ class _TabsScreenState extends State<TabsScreen> {
       setState(() {
         _favoriteMeals.remove(meal);
       });
+      _showInfoMessage('Meal is no longer a favorite');
     } else {
       setState(() {
         _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as a favorite');
       });
     }
   }
