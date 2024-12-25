757252fdd550532034dfce6531f0eebb1b3c0c32
Jessica Ernst <jessica_ernst@outlook.de>
2024-12-25 21:13:44 +0100

    add main refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_2/screens/tabs.dart';
-
-final theme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
-  textTheme: GoogleFonts.latoTextTheme(),
-);
+import 'package:meals_2/settings/color_themes.dart';
 
 void main() {
   runApp(const App());
