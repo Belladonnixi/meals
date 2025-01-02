0da503d2fb551d7cbf838168fb331d66d5c0a8e6
Jessica Ernst <jessica_ernst@outlook.de>
2025-01-02 10:39:20 +0100

    add explicit animation sliding in categories

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_2/models/category.dart';
@@ -8,36 +10,70 @@ import 'package:meals_2/providers/categories_provider.dart';
 
 final selectedCategoryProvider = StateProvider<Category?>((ref) => null);
 
-class CategorieScreen extends ConsumerWidget {
+class CategorieScreen extends ConsumerStatefulWidget {
   const CategorieScreen({super.key, required this.availableMeals});
 
   final List<Meal> availableMeals;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _CategorieScreenState createState() => _CategorieScreenState();
+}
+
+class _CategorieScreenState extends ConsumerState<CategorieScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final categoriesAsyncValue = ref.watch(categoriesProvider);
     final mealsCount = ref.watch(mealCountProvider);
 
-    return categoriesAsyncValue.when(
-      data: (categories) => GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: categoriesAsyncValue.when(
+        data: (categories) => GridView(
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: categories
+              .map((category) => CategoryGridItem(
+                    category: category,
+                    selectCategory: () => selectCategory(
+                        context, ref, category, widget.availableMeals),
+                    mealCount: mealsCount[category.id] ?? 0,
+                  ))
+              .toList(),
         ),
-        children: categories
-            .map((category) => CategoryGridItem(
-                  category: category,
-                  selectCategory: () =>
-                      selectCategory(context, ref, category, availableMeals),
-                  mealCount: mealsCount[category.id] ?? 0,
-                ))
-            .toList(),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, stack) => Center(child: Text('Error: $err')),
       ),
-      loading: () => const Center(child: CircularProgressIndicator()),
-      error: (err, stack) => Center(child: Text('Error: $err')),
+      builder: (context, child) => Padding(
+          padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+          child: child),
     );
   }
 }
